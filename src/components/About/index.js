b8def5746729e4d4fa88677ce3913c8c81f4cbae
zardron <zardron.pesquera1993@gmail.com>
2023-07-15 10:54:10 +0400

    partial template portfolio

---
+++ src/components/About/index.js
@@ -0,0 +1,89 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+  faPython,
+} from "@fortawesome/free-brands-svg-icons";
+import Loader from "react-loaders";
+import AnimatedLetters from "../AnimatedLetters";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./index.scss";
+
+const About = () => {
+  const aboutArray = "About Me".split("");
+
+  const [letterClass, setLetterClass] = useState("text-animate");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={aboutArray}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm a Bachelor of Science in Information Technology Graduate at
+            University of Cebu Lapu-Lapu and Manduae (UCLM).
+          </p>
+          <p>
+            A very ambitious developer. I love to code because if I can think
+            it, I can make it a reality.
+          </p>
+          <p>
+            Also a competitive coder with an amazing ability to develop websites
+            that are both functional and aesthetically pleasing. I have a strong
+            understanding of web standards and best practices, and I am
+            passionate about creating websites that users will find easy to use
+            and visually appealing.
+          </p>
+          <p>
+            If I need to define myself in one sentence that would be an Coding
+            Lover, and tech-obsessed!!!
+          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faPython} color="#4B8BBE" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="ball-scale-ripple-multiple" />
+    </>
+  );
+};
+
+export default About;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,84 @@
+import "./index.scss";
+import LogoS from "../../assets/images/logo-z.png";
+import LogoSubtitle from "../../assets/images/zardron_logo_white.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faGithub,
+  faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faHome,
+  faUser,
+  faEnvelope,
+  faScrewdriverWrench,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link, NavLink } from "react-router-dom";
+
+const Sidebar = () => {
+  return (
+    <>
+      <div className="nav-bar">
+        <Link className="logo" to="/">
+          <img src={LogoS} alt="Logo" />
+          <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+        </Link>
+        <nav>
+          <NavLink exact="true" activeclassname="active" to="/">
+            <FontAwesomeIcon icon={faHome} color="#00C4FF" />
+          </NavLink>
+          <NavLink activeclassname="active" className="about-link" to="/about">
+            <FontAwesomeIcon icon={faUser} color="#00C4FF" />
+          </NavLink>
+
+          <NavLink
+            activeclassname="active"
+            className="skills-link"
+            to="/skills"
+          >
+            <FontAwesomeIcon icon={faScrewdriverWrench} color="#00C4FF" />
+          </NavLink>
+
+          <NavLink
+            activeclassname="active"
+            className="contact-link"
+            to="/contact"
+          >
+            <FontAwesomeIcon icon={faEnvelope} color="#00C4FF" />
+          </NavLink>
+        </nav>
+        <ul>
+          <li>
+            <a
+              href="https://github.com/Zardron"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faGithub} color="#b9b9b9" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://ph.linkedin.com/in/zardron-angelo-pesquera-89b8961ba"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faLinkedin} color="#b9b9b9" />
+            </a>
+          </li>
+          <li>
+            <a
+              href=" https://www.facebook.com/zardron.pesquera/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faFacebook} color="#b9b9b9" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
